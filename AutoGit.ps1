2850271f28ed6daa7d8261cf88a1577059f3c01e
Paulson Hanel <Paulson.Hanel@mymhc.ca>
2021-10-05 11:29:41 -0600

    uploaded script, no other earlyer commits becasue I already did quite a bit in a different repo

---
+++ AutoGit.ps1
@@ -0,0 +1,10 @@
+﻿<# Pull the current repo to avoid discrepancys amounst code #>
+git pull
+
+<# Add the current dir, commit it with parameter specified after calling script, push it to github #>
+git add *
+git commit -m $args[0]
+git push -q
+
+<# If you don't have the authority to run the script then paste and execute the command below #>
+<# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser #>
